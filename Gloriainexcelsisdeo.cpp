ce5c5b8a03892de2ec22c201fa34dfdb80a24fbf
JackRaMotta <andreamarotta.contact@gmail.com>
2013-10-14 22:34:36 +0200

    p

--- Gloriainexcelsisdeo.cpp
+++ Gloriainexcelsisdeo.cpp
@@ -3,12 +3,8 @@ using namespace std;
 
 
 int main(){
-<<<<<<< HEAD
+
 	cout<<"ciao\n";
 	cout<<"youre a bastard";
-=======
-	cout<<"ci
-		";
->>>>>>> 228dc40506c507fe9d08e59172f509cdab8507bb
 	return 0;
 }
